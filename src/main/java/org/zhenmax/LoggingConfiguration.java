8d7714532008542039ea9816dfe7ed82abaea64e
MakZhon <abovezer0lol@gmail.com>
2024-06-09 21:56:22 +0200

    init

---
+++ src/main/java/org/zhenmax/LoggingConfiguration.java
@@ -0,0 +1,9 @@
+package org.zhenmax;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class LoggingConfiguration {
+    public static void testLog(String str) {
+        System.out.printf("Logging From Maven artifactory %s", str);
+    }
+}
